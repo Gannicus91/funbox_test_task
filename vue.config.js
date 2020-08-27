7b419c889d5f69729808eb01d1ade50fd807bdb5
m.dzhivanyan <m.dzhivanyan@brandquad.ru>
2020-08-27 21:27:27 +0400

    добавление конфигурационных файлов

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': path.join(__dirname, 'src/assets'),
+        '@images': path.join(__dirname, 'src/assets/images'),
+        '@': path.join(__dirname, 'src/'),
+      },
+    },
+  },
+  css: {
+    loaderOptions: {
+      'stylus': {
+        import: '~assets/styl/_global.styl',
+      },
+    },
+  },
+  chainWebpack: config => {
+    // удаляем prefetch плагин:
+    config.plugins.delete('prefetch');
+  },
+};
